be7c86dd83686314fdaf8740d053c7803e7bb318
Gregv21v <gregv21v@users.noreply.github.com>
2025-08-21 16:05:53 -0400

    Add three-panel layout

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders left, center, and right panels', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/left panel/i)).toBeInTheDocument();
+  expect(screen.getByText(/center panel/i)).toBeInTheDocument();
+  expect(screen.getByText(/right panel/i)).toBeInTheDocument();
 });
